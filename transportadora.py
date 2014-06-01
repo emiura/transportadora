5469b20c3ba1c3ee6489a074a5420edb31c0da77
miura <miura@debian>
2014-06-01 16:03:38 -0300

    - Adicionadas funcionalidades principais

--- transportadora.py
+++ transportadora.py
@@ -4,6 +4,7 @@
 import sys
 import easygui as eg
 import MySQLdb
+import subprocess 
 
 # connection
 db = MySQLdb.connect("localhost", "root", "rapadura", "transportadora")
@@ -12,6 +13,10 @@ db = MySQLdb.connect("localhost", "root", "rapadura", "transportadora")
 cursor = db.cursor()
 
 title = "Programa de controle de transportadora"
+
+def sendPrinter(text):
+   lpr = subprocess.Popen("/usr/bin/lpr", stdin=subprocess.PIPE)
+   lpr.stdin.write(text) 
 
 # show main menu
 def showMenu():
@@ -37,7 +42,7 @@ def showMenu():
          if (cad_type == "Rodovias"):
             sys.exit(0)
          if (cad_type == None):
-            sys.exit(0)
+            showMenu()
          
       # search
       if (action == "Pesquisa"):
@@ -58,7 +63,7 @@ def showMenu():
          if (pesq_type == "Rodovias"):
             sys.exit(0)
          if (pesq_type == None):
-            sys.exit(0)
+            showMenu()
 
       # operational
       if (action == "Operacao"):
@@ -67,29 +72,33 @@ def showMenu():
          if (op_type == "Ordem servico"):
             cadOrdens()
          if (op_type == "Viagens"):
-            sys.exit(0)
+            cadViagens()
          if (op_type == "Carga/Descarga"):
             sys.exit(0)
          if (op_type == None):
-            sys.exit(0)
+            showMenu()
 
       # report
       if (action == "Relatorios"):
          rel_entries = [ "Veiculos", "Clientes", "Funcionarios" ]
          rel_type = eg.choicebox("Escolha o tipo de Relatorio", title = title, choices = rel_entries)
          if (rel_type == "Veiculos"):
-            sys.exit(0)
+            reportVeiculos()
          if (rel_type == "Clientes"):
             sys.exit(0)
          if (rel_type == "Funcionarios"):
-            sys.exit(0)
+            reportFunc()
          if (rel_type == None):
-            sys.exit(0)
+            showMenu()
 
       # exit
       if (action == None) or (action == "Sair"):
          choice = eg.ynbox(msg = "Deseja realmente sair?", title = title, choices = ("Sim", "Não"), image = None)
          sys.exit(0)
+
+
+
+
 
 # execute query
 def execQuery(query):
@@ -101,6 +110,8 @@ def execQuery(query):
       print "Error %d: %s" % (e.args[0], e.args[1])
       db.rollback()
 
+
+
 # insert funcionario
 def cadFunc():
    msg = "Informe dados do funcionario"
@@ -109,7 +120,7 @@ def cadFunc():
    fieldValues = eg.multenterbox(msg, title, fieldNames)
    while 1:
       if fieldValues == None:
-         break
+         showMenu()
       errmsg = ""
       for i in range(len(fieldNames)):
          if fieldValues[i].strip() == "":
@@ -121,6 +132,8 @@ def cadFunc():
       funcionarios = "INSERT INTO Funcionarios(funNome, funEnder, funFone, funDtNasc, funClasse, funCateg) VALUES ('%s', '%s', '%s', '%s', '%s', '%s')" % (fieldValues[0], fieldValues[1], fieldValues[2], fieldValues[3], fieldValues[4], fieldValues[5])
       execQuery(funcionarios)
 
+
+
 # insert veiculo
 def cadVei():
    msg = "Informe dados do Veiculo"
@@ -129,7 +142,7 @@ def cadVei():
    fieldValues = eg.multenterbox(msg, title, fieldNames)
    while 1:
       if fieldValues == None:
-         break
+         showMenu()
       errmsg = ""
       for i in range(len(fieldNames)):
          if fieldValues[i].strip() == "":
@@ -140,6 +153,8 @@ def cadVei():
    if (fieldValues != None):
       veiculos = "INSERT INTO Veiculos(veiDescr, veiAno, veiPlaca, veiKm, veiCateg) VALUES ('%s', '%d', '%s', '%d', '%s')" % (fieldValues[0], int(fieldValues[1]), fieldValues[2], int(fieldValues[3]), fieldValues[4])
       execQuery(veiculos)
+
+
 
 # search Funcionario
 def pesqFunc():
@@ -158,13 +173,17 @@ def pesqFunc():
       query = "funClasse"
    if (search == "Categoria"):
       query = "funCateg"
+   if (search == None):
+      showMenu()
+
    search_string = eg.enterbox("Informe a string a pesquisar", title = title, default = "", strip=True)
    fun_query = "SELECT * FROM Funcionarios WHERE %s LIKE '%s'" % (query, "%" + search_string + "%")
    try:
       cursor.execute(fun_query)
+      db.commit()
       # Fetch all the rows 
       results = cursor.fetchall()
-      text  = "Codigo" + " | " + "Nome" + " | " + "Endereco" + " | " + "Fone" + " | " + "Data Nascimento" + " | " + "Classe" + " | " + "Categoria"
+      sql_output = []
       for row in results:
          codigo = row[0]
          nome = row[1]
@@ -173,10 +192,57 @@ def pesqFunc():
          nasc = row[4] 
          classe = row[5]
          categoria = row[6]
-         text = text + '\n' + str(codigo) + " | " + nome + " | " + endereco + " | " + fone + " | "  + str(nasc) + " | " + classe + " | " + categoria 
+         text = str(codigo) + " | " + nome + " | " + endereco + " | " + fone + " | " + str(nasc) + " | " + classe + " | " + categoria
+         sql_output.append(text)
    except MySQLdb.Error, e:
       print "Error %d: %s" % (e.args[0], e.args[1])
-   eg.textbox("Pesquisa em Funcionarios:", title = title, text = text, codebox = 1)
+
+   edit_func = eg.choicebox("Codigo - Nome - Endereco - Fone - Data Nasc. - Classe - Categoria", title = title, choices = sql_output)
+   codigo = edit_func.split("|")[0].replace(" ", "")
+   nome = edit_func.split("|")[1].strip()
+   endereco = edit_func.split("|")[2].strip()
+   fone = edit_func.split("|")[3].replace(" ", "")
+   nascimento = edit_func.split("|")[4].replace(" ", "")
+   print
+   classe = edit_func.split("|")[5].replace(" ", "")
+   categoria = edit_func.split("|")[6].replace(" ", "")
+
+   msg = "Deseja editar, deletar o registro ou Continuar?"
+   choices = ["Editar", "Deletar", "Continuar"]
+   edit_or_delete = eg.buttonbox(msg,choices=choices)
+   if (edit_or_delete == "Editar"): 
+      msg = "Atualizacao dos registros"
+      fieldNames = [ "Codigo", "Nome", "Endereco", "Fone", "Data Nascimento", "Classe", "Categoria" ]  
+      fieldValues = [ codigo, nome, endereco, fone, nascimento, classe, categoria ]
+      fieldValues = eg.multenterbox(msg, title, fieldNames, fieldValues)
+      while 1:
+         if fieldValues == None:
+            break
+         errmsg = ""
+         for i in range(len(fieldNames)):
+            if fieldValues[i].strip() == "":
+               errmsg = errmsg + ('"%s" eh requerido'  % fieldNames[i])
+         if errmsg == "":
+            break
+         fieldValues = eg.multenterbox(errmsg, title, fieldNames, fieldValues)
+      if (fieldValues != None):
+         try:
+            print cursor.execute("UPDATE Funcionarios SET funNome = '%s' WHERE funCodigo = '%d'" % (fieldValues[1], int(codigo)))
+            print cursor.execute("UPDATE Funcionarios SET funEnder = '%s' WHERE funCodigo = '%d'" % (fieldValues[2], int(codigo)))
+            print cursor.execute("UPDATE Funcionarios SET funFone = '%s' WHERE funCodigo = '%d'" % (fieldValues[3], int(codigo)))
+            print cursor.execute("UPDATE Funcionarios SET funDtNasc = '%s' WHERE funCodigo = '%d'" % (fieldValues[4], int(codigo)))
+            print cursor.execute("UPDATE Funcionarios SET funClasse = '%s' WHERE funCodigo = '%d'" % (fieldValues[5], int(codigo)))
+            print cursor.execute("UPDATE Funcionarios SET funCateg = '%s' WHERE funCodigo = '%d'" % (fieldValues[6], int(codigo)))
+            db.commit()
+         except MySQLdb.Error, e:
+            print "Error %d: %s" % (e.args[0], e.args[1])
+
+   if(edit_or_delete == "Deletar"):
+      try:
+         cursor.execute("DELETE FROM Funcionarios WHERE funCodigo = '%d'" % (int(codigo)))
+         db.commit()
+      except MySQLdb.Error, e:
+         print "Error %d: %s" % (e.args[0], e.args[1])
 
 
 # search veiculo
@@ -194,13 +260,17 @@ def pesqVei():
       query = "veiKm"
    if (search == "Categoria"):
       query = "veiCateg"
+   if (search == None):
+      showMenu()
+
    search_string = eg.enterbox("Informe a string a pesquisar", title = title, default = "", strip=True)
    vei_query = "SELECT * FROM Veiculos WHERE %s LIKE '%s'" % (query, "%" + search_string + "%")
    try:
       cursor.execute(vei_query)
+      db.commit()
       # Fetch all the rows 
       results = cursor.fetchall()
-      text  = "Codigo\t" + " | " + "Descricao\t" + " | " + "Ano\t" + " | " + "Placa\t" + " | " + "Quilometragem\t" + " | " + "Categoria\t"
+      sql_output = []
       for row in results:
          codigo = row[0]
          descricao = row[1]
@@ -208,22 +278,72 @@ def pesqVei():
          placa = row[3]
          km = row[4]
          categ = row[5]
-         text = text + '\n' + str(codigo) + " | " + descricao + " | " + str(ano) + " | " + placa + " | "  + str(km) + " | " + categ 
+         text = str(codigo) + " | " + descricao + " | " + str(ano) + " | " + placa + " | "  + str(km) + " | " + categ 
+         sql_output.append(text)
    except MySQLdb.Error, e:
       print "Error %d: %s" % (e.args[0], e.args[1])
-   eg.textbox("Pesquisa em veiculos:", title = title, text = text, codebox = 1)
+
+   edit_veic = eg.choicebox("Codigo - Descricao - Ano - Placa - KM - Categoria", title = title, choices = sql_output)
+
+   codigo = edit_veic.split("|")[0].replace(" ", "")
+   descricao = edit_veic.split("|")[1].strip()
+   ano = edit_veic.split("|")[2].replace(" ", "")
+   placa = edit_veic.split("|")[3].replace(" ", "")
+   km = edit_veic.split("|")[4].replace(" ", "")
+   categ = edit_veic.split("|")[5].replace(" ", "")
+
+   msg = "Deseja editar ou deletar o registro ou Continuar?"
+   choices = ["Editar", "Deletar", "Continuar"]
+   edit_or_delete = eg.buttonbox(msg,choices=choices)
+   if (edit_or_delete == "Editar"): 
+      msg = "Atualizacao dos registros"
+      fieldNames = [ "Codigo", "Descricao", "Ano", "Placa", "Quilometragem", "Categoria" ]  
+      fieldValues = [ codigo, descricao, ano, placa, km, categ ]
+      fieldValues = eg.multenterbox(msg, title, fieldNames, fieldValues)
+      while 1:
+         if fieldValues == None:
+            break
+         errmsg = ""
+         for i in range(len(fieldNames)):
+            if fieldValues[i].strip() == "":
+               errmsg = errmsg + ('"%s" eh requerido'  % fieldNames[i])
+         if errmsg == "":
+            break
+         fieldValues = eg.multenterbox(errmsg, title, fieldNames, fieldValues)
+      if (fieldValues != None):
+         try:
+            print cursor.execute("UPDATE Veiculos SET veiDescr = '%s' WHERE veiCodigo = '%d'" % (fieldValues[1], int(codigo)))
+            print cursor.execute("UPDATE Veiculos SET veiAno = '%d' WHERE veiCodigo = '%d'" % (int(fieldValues[2]), int(codigo)))
+            print cursor.execute("UPDATE Veiculos SET veiPlaca = '%s' WHERE veiCodigo = '%d'" % (fieldValues[3], int(codigo)))
+            print cursor.execute("UPDATE Veiculos SET veiKm = '%d' WHERE veiCodigo = '%d'" % (int(fieldValues[4]), int(codigo)))
+            print cursor.execute("UPDATE Veiculos SET veiCateg = '%s' WHERE veiCodigo = '%d'" % (fieldValues[5], int(codigo)))
+            db.commit()
+         except MySQLdb.Error, e:
+            print "Error %d: %s" % (e.args[0], e.args[1])
+   if(edit_or_delete == "Deletar"):
+      try:
+         cursor.execute("DELETE FROM Veiculos WHERE veiCodigo = '%d'" % (int(codigo)))
+         db.commit()
+      except MySQLdb.Error, e:
+         print "Error %d: %s" % (e.args[0], e.args[1])
+
 
 # register ordens
 def cadOrdens():
-   last_entry = cursor.execute("select ordNumero from Ordens order by ordNumero DESC limit 1")
-   last_entry += 1
+   try:
+      cursor.execute("select ordNumero from Ordens order by ordNumero DESC limit 1")
+      db.commit()
+      num = cursor.fetchone()
+      last_entry = int(num[0]) + 1
+   except MySQLdb.Error, e:
+      print "Error %d: %s" % (e.args[0], e.args[1])
    msg = "Informe dados do Pedido"
    fieldNames = [ "Data", "Quantidade itens", "Valor total", "Codigo Origem", "Codigo Destino", "Codigo Remetente", "Codigo Destinatario", "Codigo Funcionario", "ICMS", "ISS", "Tipo"  ]  
    fieldValues = []
    fieldValues = eg.multenterbox(msg, title, fieldNames)
    while 1:
       if fieldValues == None:
-         break
+         showMenu()
       errmsg = ""
       for i in range(len(fieldNames)):
          if fieldValues[i].strip() == "":
@@ -237,6 +357,92 @@ def cadOrdens():
 
 
 
+
+# register viagens
+def cadViagens():
+   try:
+      cursor.execute("select viaNumero from Viagens order by viaNumero DESC limit 1")
+      db.commit()
+      num = cursor.fetchone()
+      last_entry = int(num[0]) + 1
+      print last_entry
+   except MySQLdb.Error, e:
+      print "Error %d: %s" % (e.args[0], e.args[1])
+   msg = "Informe dados da Viagem:"
+   fieldNames = [ "Data", "Observacoes", "KM Inicial", "KM Final", "Codigo Veiculo", "Codigo Funcionario", "Codigo da Rota" ]  
+   fieldValues = []
+   fieldValues = eg.multenterbox(msg, title, fieldNames)
+   while 1:
+      if fieldValues == None:
+         showMenu()
+      errmsg = ""
+      for i in range(len(fieldNames)):
+         if fieldValues[i].strip() == "":
+            errmsg = errmsg + ('"%s" eh requerido'  % fieldNames[i])
+      if errmsg == "":
+         break
+      fieldValues = eg.multenterbox(errmsg, title, fieldNames, fieldValues)
+   if (fieldValues != None):
+      viagens = "INSERT INTO Viagens(viaNumero, viaData, viaObs, viaKmIni, viaKmFim, veiCodigo, funCodigo, rtaCodigo) VALUES ('%d', '%s', '%s', '%d', '%d', '%d', '%d', '%d' )" % (last_entry, fieldValues[0], fieldValues[1], int(fieldValues[2]), int(fieldValues[3]), int(fieldValues[4]), int(fieldValues[5]), int(fieldValues[6]))
+      print execQuery(viagens)
+
+
+
+# report Funcionarios
+def reportFunc():
+   msg = "Relatorio de Funcionarios"
+   func_query = "SELECT * FROM Funcionarios"
+   try:
+      cursor.execute(func_query)
+      db.commit()
+      # Fetch all the rows 
+      results = cursor.fetchall()
+      text  = "| \t" + "Codigo \t" + " | \t" + "Nome \t" + " | \t" + "Endereco \t" + " | \t" + "Fone \t" + " | \t" + "Data Nascimento \t" + " | \t" + "Classe \t" + " | \t" + "Categoria \t" + " |"
+      for row in results:
+         codigo = row[0]
+         nome = row[1]
+         endereco = row[2]
+         fone = row[3]
+         nasc = row[4] 
+         classe = row[5]
+         categoria = row[6]
+         text = text + '\n' + str(codigo) + " - " + nome + " - " + endereco + " - " + fone + " - " + str(nasc) + " - " + classe + " - " + categoria 
+   except MySQLdb.Error, e:
+      print "Error %d: %s" % (e.args[0], e.args[1])
+   eg.textbox("Pesquisa em Funcionarios:", title = title, text = text, codebox = 1)
+   msg = "Deseja imprimir?"
+   if eg.ccbox(msg, title): 
+      sendPrinter(text)
+   else:
+      showMenu()
+
+
+# report Veiculos
+def reportVeiculos():
+   msg = "Relatorio de Veiculos"
+   veic_query = "SELECT * FROM Veiculos"
+   try:
+      cursor.execute(veic_query)
+      db.commit()
+      # Fetch all the rows 
+      results = cursor.fetchall()
+      text = "| \t" + "Codigo \t" + " | \t" + "Descricao \t" + " | \t" + "Ano \t" + " | \t" + "Placa \t" + " | \t"  + "Quilometragem \t" + " | \t" + "Categoria \t" + " |"
+      for row in results:
+         codigo = row[0]
+         descricao = row[1]
+         ano = row[2]
+         placa = row[3]
+         km = row[4]
+         categ = row[5]
+         text = text + '\n' + str(codigo) + " - " + descricao + " - " + str(ano) + " - " + placa + " - "  + str(km) + " - " + categ 
+   except MySQLdb.Error, e:
+      print "Error %d: %s" % (e.args[0], e.args[1])
+   eg.textbox("Pesquisa em Veiculos:", title = title, text = text, codebox = 1)
+   msg = "Deseja imprimir?"
+   if eg.ccbox(msg, title): 
+      sendPrinter(text)
+   else:
+      showMenu()
 
 
 # main
